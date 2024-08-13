81d90786905bfc8734f1872546d65f12a2f97d8f
Nimmo-san <nat25oloman@gmail.com>
2024-08-13 19:37:04 +0100

    Connected Flask application to Postgresql database

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+def get_db_connection():
+	conn = psycopg2.connect(dbname='covid19_data', user='nemo', password='nemo', host='localhost')
+	return conn
+
+
+
+
+
 @app.route('/')
 def home():
 	return jsonify(message="Welcome to the covid19 data dashboard")
