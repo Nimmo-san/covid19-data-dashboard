fd72070277da455bca28e70f6f86e27a2463ad4e
Nimmo-san <nat25oloman@gmail.com>
2024-08-14 10:32:39 +0100

    Miscellaneous changes to frontend and data ingestion file

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,26 +1,26 @@
-import requests 
+import requests
 import pandas as pd
 import psycopg2
 
-# Fetching the data from the api
+# Fetch data from the COVID-19 API
 url = 'https://api.covid19api.com/summary'
-reponse = requests.get(url)
+response = requests.get(url)
 data = response.json()
 
-
-# Converting the data into a DataFrame
+# Convert data to a DataFrame
 countries_data = data['Countries']
 df = pd.DataFrame(countries_data)
 
-# pushing the data into postgresql
+# Insert data into PostgreSQL
 conn = psycopg2.connect(
-	dbname='covid19_data',
-	user='nemo',
-	password='nemo',
-	host='localhost')
-
+    dbname='covid19_data',
+    user='yourusername',
+    password='yourpassword',
+    host='localhost'
+)
 cursor = conn.cursor()
 
+# Iterating through the data and adding it to the covid_stats table
 for _, row in df.iterrows():
     cursor.execute("""
         INSERT INTO covid_stats (country, total_cases, total_deaths, total_recovered)
@@ -29,5 +29,5 @@ for _, row in df.iterrows():
 
 conn.commit()
 cursor.close()
-conn.close() 
-	
+conn.close()
+
